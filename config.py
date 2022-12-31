8da5fad0212f4657c7dda97d3287fbdd1cb40701
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2022-12-31 13:18:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config:
     COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Forward_data')
     SESSION = os.environ.get("SESSION", "BQAZ3mUAqtxZPNbgNr3x2WWAuR_s_RRezAmQPCgMvGw4Y9Tp4LUnlp30OGqvWALCN1vc_fxavmRgtfRFMPijmkajJL7Kiwsz2HPwmpLYmPFg1ypXhRCVgAcYfTWAEm48UTUY8741NTD7nQc8oECZt41hA8FwEkPNPe-8FwlrHpYIm6q3gjwNEDlaC4Qfjrwf4doh1582L5Cp_z186cgxynuLBjTHjr-RAD98ATtNpMxXD4bykcPR1sbezOMhnFfHHZAMhjbAFytDDumqupVS3TaxZCHfoaeU3w84gE3KTaQ2Fu4EKsxNlXoa4aMj16zvgTp7P_Ud6DNT58v5lFhkZnfqMLHlxgAAAAA8mqdNAA")
     TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1001525314409"))
-    BOT_USERNAME= os.environ.get("BOT_USERNAME", @TheMediaForwardBot)
+    BOT_USERNAME= os.environ.get("BOT_USERNAME", "@TheMediaForwardBot")
 
 
 def LOGGER(name: str) -> logging.Logger:
