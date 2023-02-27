56b2a22bcd6688c0437517e9f300f297bb55b5c4
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2023-02-27 23:13:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5527448498:AAH-8panVukNPyY7AhBMhPBj-yjRSpqilzI") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "Forward_BOT") 
     OWNER_ID = os.environ.get("OWNER_ID", "1016768333")
-    DATABASE_URI = os.environ.get("DATABASE_URI", "")
+    DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://gtm:gtm@cluster0.9zsgtay.mongodb.net/?retryWrites=true&w=majority")
     DATABASE_NAME = os.environ.get("DATABASE_NAME","Cluster0")
     COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Forward_data')
     SESSION = os.environ.get("SESSION", "BQAZ3mUARGPfzi_Jovf6ryUN-hxQFS4kSDiDMWiAk2arhfBiANxF0paZIdG_hVl1FSuylVVAoIqSFboawt3YoSuG4AMLBq76tIYqed6yT0nZVX0o3GtKRxu-9QsDapLUC8eN8bTfn6wLwOcTrvrjJoXeomzvMQqrhaTNNDTLN25f77LHP-X5tlhvTqCPh0aLsdTxjHm49zd45yzfzmU6Z-tpobDKwDnJHVQ9Zoy7-mfuDiXkzsUfDLBblyvmRkopN5_oIx8uuBuIV_wNlr4pxcWC5Cb05E6WZsG5CPGKYq1mdoRYG-m-EXeN5CmkPgnZvweDQ6FaZFD-Cf-8oz1kK0UcluJSLAAAAAA8mqdNAA")
